26c6338c0e36124945262b3ec8a8c65fb20a93c3
Aorp2008 <123627593+Aorp2008@users.noreply.github.com>
2023-08-20 14:07:26 +0800

    Update easyimg.sh

--- easyimg.sh
+++ easyimg.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 if ! command -v docker &> /dev/null
 then
     wget -O docker.sh https://raw.githubusercontent.com/Aorp2008/Airp_sh/main/docker.sh && chmod +x docker.sh && clear && ./docker.sh
@@ -13,8 +12,23 @@ then
 
 fi
 
-sudo mkdir -p /root/docker/easyimg
+if ! command -v lsof &> /dev/null
+then
+    echo "lsof is not installed. Installing now..."
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        sudo apt-get install lsof
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        brew install lsof
+    fi
+fi
 
+if lsof -Pi :5588 -sTCP:LISTEN -t >/dev/null ; then
+    port=$(shuf -i 2000-65000 -n 1)
+else
+    port=5588
+fi
+
+sudo mkdir -p /root/docker/easyimg
 
 cat << EOF > /root/docker/easyimg/docker-compose.yml
 version: '3.3'
@@ -23,7 +37,7 @@ services:
     image: ddsderek/easyimage:latest
     container_name: easyimage
     ports:
-      - '5588:80'
+      - '$port:80'
     environment:
       - TZ=Asia/Shanghai
       - PUID=1000
@@ -34,8 +48,6 @@ services:
     restart: unless-stopped
 EOF
 
-
-
 cd /root/docker/easyimg && docker-compose up -d
 
-echo "安装完成请使用ip:5588访问"
+echo "安装完成请使用ip:$port访问"
