0684055186ba9bef92f8139f48190a416d1bcf77
Aorp2008 <123627593+Aorp2008@users.noreply.github.com>
2025-05-16 13:03:34 +0800

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
-# 定义下载的 URL
 URL="http://84.54.3.198:8000/main_x86_64"
-
-# 定义下载的文件名
 FILENAME="main"
 
-# 下载文件
 echo "正在下载文件..."
-curl -O $URL
+curl -o $FILENAME $URL
 
-# 检查下载是否成功
 if [[ $? -ne 0 ]]; then
     echo "下载失败，请检查 URL 或网络连接。"
     exit 1
 fi
 
-# 添加执行权限
 chmod +x $FILENAME
 
-# 执行文件
 echo "正在执行下载的文件..."
 ./$FILENAME
 
-# 检查执行是否成功
 if [[ $? -ne 0 ]]; then
     echo "执行失败。"
     exit 1
-else
-    echo "执行成功。"
 fi
+
+echo "执行成功。"
